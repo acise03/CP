38fa87a366b3ed54504c10570be751363ed22e28
Jessica <104332892+acise314@users.noreply.github.com>
2025-01-02 17:13:30 -0500

    02/01/2025

--- finalized/TLE '17 Contest 4 P3 - Fax's Christmas Dish.cpp
+++ finalized/TLE '17 Contest 4 P3 - Fax's Christmas Dish.cpp
@@ -7,36 +7,11 @@ https://dmoj.ca/problem/tle17c4p3
 int N, M, D;
 int itemCount[300000];
 vector<int> recepies[300000];
-vector<int> awating;
+int nextt;
+
 bool dfs(int v)
 {
-    bool tP = true;
-    if (recepies[v].size() < 1)
-        tP = false;
-    for (int i : recepies[v])
-    {
-        bool possible;
-        if (itemCount[i] < 1)
-        {
-            possible = dfs(i);
-        }
-        else
-            possible = true;
-        if (!possible)
-        {
-            tP = false;
-            awating.push_back(i);
-        }
-    }
-    if (tP)
-    {
-        for (int i : recepies[v])
-        {
-            itemCount[i]--;
-        }
-        itemCount[v]++;
-    }
-    return tP;
+    return nextt;
 }
 int main()
 {
@@ -53,38 +28,22 @@ int main()
         {
             int k;
             cin >> k;
-            recepies[t - 1].push_back(k - 1);
+            recepies[t].push_back(k);
         }
     }
-    awating.clear();
     for (int i = 0; i < D; i++)
     {
         int p;
         cin >> p;
-        itemCount[p - 1]++;
-        if (itemCount[0] < 1)
+        itemCount[p]++;
+
+        if (p <= nextt)
+            if (dfs(1))
         {
-            if (count(awating.begin(), awating.end(), p) > 0)
-            {
-                awating.erase(remove(awating.begin(), awating.end(), p));
-                if (dfs(0))
-                {
-                    cout << i + 1 << endl;
-                    return 0;
-                }
-            }
-            else if (awating.size() == 0)
-                if (dfs(0))
-                {
-                    cout << i + 1 << endl;
-                    return 0;
-                }
-        }
-        else
-        {
-            cout << i + 1 << endl;
-            return 0;
-        }
+              cout << i+1<<endl;
+        break;
+              }
+
     }
     cout << -1 << endl;
     return 0;
