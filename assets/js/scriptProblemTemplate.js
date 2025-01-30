66958d1494199cbeb78a880b7b72107c09d8606b
acise314 <puyuhuan1@gmail.com>
2025-01-30 15:19:11 -0500

    r

--- assets/js/scriptProblemTemplate.js
+++ assets/js/scriptProblemTemplate.js
@@ -47,10 +47,8 @@ document.addEventListener('DOMContentLoaded', function () {
 async function runCode() {
     const code = editor.getValue();
     const outputElement = document.getElementById('output');
-    const runTimeElement = document.getElementById('runtime');
     const userInput = document.getElementById('user-input').value;
     outputElement.textContent = 'Running...';
-    runTimeElement.textContent = `< b > Runtime:</b > ...Running...`;
     try {
         const response = await fetch('https://emkc.org/api/v2/piston/execute', {
             method: 'POST',
