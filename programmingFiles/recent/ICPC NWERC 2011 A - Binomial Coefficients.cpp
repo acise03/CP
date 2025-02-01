ea1caaad4cda9dd0dc3c18ac296428a2784b3e62
Jessica <puyuhuan1@gmail.com>
2025-01-31 23:55:40 -0500

    sadf

--- programmingFiles/recent/ICPC NWERC 2011 A - Binomial Coefficients.cpp
+++ programmingFiles/recent/ICPC NWERC 2011 A - Binomial Coefficients.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define int long long
 int T, m;
 
 int factorial(int n) {
@@ -16,7 +16,7 @@ int choose(int a, int b) {
 }
 
 
-int main() {
+signed main() {
     cin >> T;
 
     for (int _ = 0; _ < T; _++) {
@@ -25,26 +25,22 @@ int main() {
         string out = "";
 
         for (int k = 1; (choose(2 * (k), k) <= m); k++) {
-            int lo = 2*k-1;
-            int hi = 10000000000000000;
-            int res = -1;
+            int lo = 2 * k - 1;
+            int hi = 10;
 
-            while (lo <= hi) {
-                int mid = (hi + lo) / 2;
+            while (lo < hi) {
+                int mid = lo + (hi - lo + 1) / 2;
                 int v = choose(mid, k);
-                if (v == m) {
-                    res = mid;
-                    break;
-                } else if (v > hi) {
+                if (v >= hi) {
                     hi = mid - 1;
                 } else {
                     lo = mid + 1;
                 }
             }
-            if (res != -1) {
-                out += "(" + to_string(res) + "," + to_string(k) + ")\n";
-                if (2 * k != res) {
-                    out += "(" + to_string(res) + "," + to_string(res - k) + ")\n";
+            if (choose(lo, k) == m) {
+                out += "(" + to_string(lo) + "," + to_string(k) + ")\n";
+                if (2 * k != lo) {
+                    out += "(" + to_string(lo) + "," + to_string(lo - k) + ")\n";
                 }
                 possibles++;
             }
